ce58c52267399c01e3fccd804f8f3022f74d55b1
arieldev <adrielmon4@gmail.com>
2025-04-29 19:59:23 -0500

    Agregar multiplicacion y division

--- calculadora.js
+++ calculadora.js
@@ -8,3 +8,14 @@ function resta(a, b) {
 
 suma(5, 3); // 8
 resta(5, 3); // 2
+
+function multiplicacion(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    if (b === 0) {
+        throw new Error("No se puede dividir entre cero");
+    }
+    return a / b;
+}
